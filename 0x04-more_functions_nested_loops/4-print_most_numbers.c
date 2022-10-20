4084f32777e1ff4c7260acb589f5d6f6e799f919
kevincode06 <maloukevin6@gmail.com>
2022-10-20 10:45:57 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,12 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-			_putchar(c);
+	{
+		contunue;
+	}
+	{
+		_putchar(c + '0');
+	}
 	}
 	_putchar('\n');
 }
