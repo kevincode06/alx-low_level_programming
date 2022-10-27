b77181516d26dba4a8b8f608c1f53d321bfcec7d
kevincode06 <maloukevin6@gmail.com>
2022-10-26 23:56:56 -0700

    new of the new task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,12 +2,12 @@
 
 /**
  * _strncpy - copys a string with n
- * @dest : copy to
+ * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
- * Return:dest
+ * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
